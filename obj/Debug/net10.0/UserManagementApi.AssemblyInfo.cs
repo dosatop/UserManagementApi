04f0bd331da4dd4d3bee531ce498f19b273e3e01
AJ-Stilez <adenusijoseph0@gmail.com>
2026-07-29 09:14:45 +0100

    Added Docker file for deployment

--- obj/Debug/net10.0/UserManagementApi.AssemblyInfo.cs
+++ obj/Debug/net10.0/UserManagementApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UserManagementApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+20cc4df1b237687c6c1e27f67dc8a1a89e547be5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1ddfe5d22953987a78b017ac81bbbfb7ca96b556")]
 [assembly: System.Reflection.AssemblyProductAttribute("UserManagementApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UserManagementApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
