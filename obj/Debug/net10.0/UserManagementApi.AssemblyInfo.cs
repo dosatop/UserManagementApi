9885281d10951ce923ed6158067d2ac58c3f4db7
AJ-Stilez <adenusijoseph0@gmail.com>
2026-07-29 09:40:09 +0100

    Compiled using docker

--- obj/Debug/net10.0/UserManagementApi.AssemblyInfo.cs
+++ obj/Debug/net10.0/UserManagementApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UserManagementApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1ddfe5d22953987a78b017ac81bbbfb7ca96b556")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7597a0c7f44237a2fd9fc9d2644f1e432fb2626c")]
 [assembly: System.Reflection.AssemblyProductAttribute("UserManagementApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UserManagementApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
