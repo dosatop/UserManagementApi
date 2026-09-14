8345b0e373764e071d14e95d564b121f17374db1
AJ-Stilez <adenusijoseph0@gmail.com>
2026-09-14 19:15:36 +0100

    feat(admin): Configured the api to accept enum names

--- Program.cs
+++ Program.cs
@@ -11,10 +11,19 @@ using Resend;
 using UserManagementApi.Services.Interfaces;
 using UserManagementApi.Configuration;
 using Quartz;
+using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddControllers();
+builder.Services
+    .AddControllers()
+    .AddJsonOptions(options =>
+    {
+        options.JsonSerializerOptions.Converters.Add(
+            new JsonStringEnumConverter()
+        );
+    });
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(options =>
 {
@@ -152,14 +161,6 @@ builder.Services.AddQuartzHostedService(options =>
 {
     options.WaitForJobsToComplete = true;
 });
-builder.Services
-    .AddControllers()
-    .AddJsonOptions(options =>
-    {
-        options.JsonSerializerOptions.Converters.Add(
-            new JsonStringEnumConverter()
-        );
-    });
 
 
 builder.Services.Configure<SeedAdminSettings>(
